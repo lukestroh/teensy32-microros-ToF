0ca92695cc76a7d21ba0173188b5a3fa85ed86e6
lukestroh <luke.strohbehn@gmail.com>
2024-04-08 13:04:55 -0700

    added system status check for pinging micro_ros_agent

--- include/ToFData.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TOF_DATA_MSG_H__
-#define __TOF_DATA_MSG_H__
-
-typedef struct teensy32_microros_msgs__msg__ToFData{
-    float tof0;
-    float tof1;
-} teensy32_microros_msgs__msg__ToFData;
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+/*
+
+Connection reset code from: https://github.com/micro-ROS/micro_ros_arduino/blob/humble/examples/micro-ros_reconnection_example/micro-ros_reconnection_example.ino
+*/
+
 #include <Arduino.h>
 #include <micro_ros_platformio.h>
 
@@ -24,14 +29,23 @@ rcl_timer_t timer;
 const int led_pin = 13;
 unsigned long last_time = 0;
 
+enum SystemState {
+    AGENT_WAIT,
+    AGENT_AVAILABLE,
+    AGENT_CONNECTED,
+    AGENT_DISCONNECTED
+} system_state;
+
 void error_loop(void) {
+    /* Error loop LED indicator */
     while (true) {
         digitalWrite(led_pin, !digitalRead(led_pin));
-        delay(5000);
+        delay(1000);
     }
 }
 
 void timer_callback(rcl_timer_t* timer, int64_t last_call_time) {
+    /* Publisher timer callback method */
     RCLC_UNUSED(last_call_time);
     if (timer != NULL) {
         RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
@@ -40,22 +54,14 @@ void timer_callback(rcl_timer_t* timer, int64_t last_call_time) {
     }
 }
 
-void setup() {
-    Serial.begin(115200);
-    set_microros_serial_transports(Serial);
-    // set_microros_transports();
-
-    pinMode(led_pin, OUTPUT);
-    digitalWrite(led_pin, LOW);
-    
-    delay(2000);
-
-
+bool create_rcl_entities() {
+    /* Create the microROS entities */
+    // Init allocator
     allocator = rcl_get_default_allocator();
 
     // create init_options
     RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-
+    
     // create node
     RCCHECK(rclc_node_init_default(&node, "teensy32_microros", "", &support));
 
@@ -66,7 +72,7 @@ void setup() {
         ROSIDL_GET_MSG_TYPE_SUPPORT(teensy32_tof_msgs, msg, ToFData),
         "microROS/tof_data"
     ));
-
+    
     // create timer
     const unsigned int timer_period {10};
     RCCHECK(rclc_timer_init_default(
@@ -80,24 +86,89 @@ void setup() {
     RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
     RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
+    return true;
+}
+
+void destroy_rcl_entities() {
+    /* Destroy the microROS-related entities */
+    rmw_context_t* rmw_context = rcl_context_get_rmw_context(&support.context);
+    (void) rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
+
+    rcl_publisher_fini(&publisher, &node);
+    rcl_timer_fini(&timer);
+    rclc_executor_fini(&executor);
+    rcl_node_fini(&node);
+    rclc_support_fini(&support);
+}
+
+void execute_every_n_ms(int64_t ms, SystemState system_state) {
+    /* Method for periodically pinging the micro_ros_agent */
+    do {
+        static volatile int64_t init = -1;
+        if (init == -1) {
+            init = uxr_millis();
+        }
+        if (uxr_millis() - init > ms) {
+            system_state;
+            init = uxr_millis();
+        }
+    }
+    while (0);
+}
+
+void setup() {
+    Serial.begin(115200);
+    set_microros_serial_transports(Serial); // PlatformIO
+
+    pinMode(led_pin, OUTPUT);
+    digitalWrite(led_pin, LOW);
+
+    system_state = AGENT_WAIT;
+
     msg.tof0 = 0.0;
     msg.tof1 = 0.0;
 }
 
 void loop() {
-    // delay(100);
-    unsigned long now = millis();
-    if (now - last_time > 9900) {
-        digitalWrite(led_pin, HIGH); 
+    // Check system state for connection status to micro_ros_agent
+    switch (system_state) {
+        case AGENT_WAIT:
+            execute_every_n_ms(500, system_state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_AVAILABLE : AGENT_WAIT);
+            break;
+        case AGENT_AVAILABLE:
+            system_state = (true == create_rcl_entities()) ? AGENT_CONNECTED : AGENT_WAIT;
+            if (system_state == AGENT_WAIT) {
+                destroy_rcl_entities();
+            }
+            break;
+        case AGENT_CONNECTED:
+            // Publish message
+            execute_every_n_ms(200, system_state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED);
+            if (system_state == AGENT_CONNECTED) {
+                RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+            }
+            break;
+        case AGENT_DISCONNECTED:
+            destroy_rcl_entities();
+            system_state = AGENT_WAIT;
+            break;
+        default:
+            break;
     }
-    if (now - last_time > 10000) {
-        digitalWrite(led_pin, LOW); 
-        last_time = now;
-    }
-    RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
-       
+
+    // LED control
+    if (system_state == AGENT_CONNECTED) {
+            unsigned long now = millis();
+        if (now - last_time > 9900) {
+            digitalWrite(led_pin, HIGH); 
+        }
+        if (now - last_time > 10000) {
+            digitalWrite(led_pin, LOW); 
+            last_time = now;
+        }
+    }     
 }
 
 /* Running this script:
-After restarting the host and the board, I started it with ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/serial/by-id/usb-STMicroelectronics_STM32_STLink_0672FF3530384E5043161319-if02 -v6 ROS_DOMAIN_ID=0 and only then I plugged in the board. In another terminal window i did export ROS_DOMAIN_ID=0 followed by ros2 topic list. Now it works, thanks!
+ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyACM0 ROS_DOMAIN_ID=0
 */
