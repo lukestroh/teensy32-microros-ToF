d329b530f8a80aaae9d68d0cbcf2d1ebd6da05cd
lukestroh <luke.strohbehn@gmail.com>
2024-05-27 22:40:03 -0700

    made initial attempt to get multi-sensor, looks like I may need continuous mode

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ Connection reset code from: https://github.com/micro-ROS/micro_ros_arduino/blob/
 #include <teensy32_tof_msgs/msg/to_f_data.h>
 #include <teensy32_tof_msgs/msg/to_f_data_array.h>
 
+/* microROS setup */
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if ((temp_rc != RCL_RET_OK)){}}
 
@@ -33,10 +34,24 @@ rcl_allocator_t allocator;
 rcl_node_t node;
 rcl_timer_t timer;
 
+
+/* ToF setup */
+// Define which Wire objects to use, may depend on platform
+// or on your configurations.
+#define SENSOR1_WIRE Wire
+#define SENSOR2_WIRE Wire
+#if defined(WIRE_IMPLEMENT_WIRE1)
+#define SENSOR3_WIRE Wire1
+#define SENSOR4_WIRE Wire1
+#else
+#define SENSOR3_WIRE Wire
+#define SENSOR4_WIRE Wire
+#endif
+
 const int led_pin = 13;
 unsigned long last_time = 0;
 const uint8_t xshutpins[2] = {11, 12};
-const uint8_t num_devices = sizeof(xshutpins) / sizeof(xshutpins[0]);
+// const uint8_t num_sensors = sizeof(xshutpins) / sizeof(xshutpins[0]);
 
 // VL53L0X setup
 Adafruit_VL53L0X tof0 = Adafruit_VL53L0X();
@@ -44,9 +59,29 @@ Adafruit_VL53L0X tof1 = Adafruit_VL53L0X();
 Adafruit_VL53L0X tofs[2] = {tof0, tof1};
 uint32_t tof0_addr = 0x2A;
 uint32_t tof1_addr = 0x2B;
+uint32_t tof_addrs[2] = {tof0_addr, tof1_addr};
 VL53L0X_RangingMeasurementData_t tofdata0;
 VL53L0X_RangingMeasurementData_t tofdata1;
 VL53L0X_RangingMeasurementData_t data_structs[2];
+
+typedef struct {
+    Adafruit_VL53L0X *psensor; // pointer to sensor object
+    TwoWire *pwire;
+    int id;     // sensor id
+    int shutdown_pin;
+    int interrupt_pin;
+    Adafruit_VL53L0X::VL53L0X_Sense_config_t sensor_config;
+    uint16_t range;
+    uint8_t status;
+} sensor_t;
+
+Adafruit_VL53L0X sensor1;
+Adafruit_VL53L0X sensor2;
+sensor_t sensors[] = {
+    {&sensor1, &Wire, 0x2A, 11, 26, Adafruit_VL53L0X::VL53L0X_SENSE_DEFAULT, 0, 0},
+    {&sensor2, &Wire, 0x2B, 12, 27, Adafruit_VL53L0X::VL53L0X_SENSE_DEFAULT, 0, 0}
+};
+const uint8_t num_sensors = sizeof(sensors) / sizeof(sensors)[0];
 
 // microROS system state
 enum SystemState {
@@ -57,10 +92,10 @@ enum SystemState {
 } system_state;
 
 // Software reset
-void (* reset_cpu)(void) = 0;
+void (* reset_cpu)(void) = 0; // this doesn't work!
 
 void error_loop(void) {
-    /* Error loop LED indicator */ 
+    /* Error loop LED indicator */
     Serial.println(F("Error, resetting in 5s... "));
     uint32_t time = millis();
     while (millis() - time < 5000) {
@@ -151,70 +186,68 @@ void execute_every_n_ms(int64_t ms, SystemState system_state) {
 void reset_tof_sensors() {
     /* Get the I2C ToF sensors ready to read data */
     // Disable everything by driving XSHUT pins low
-    for (int8_t i=0; i<num_devices; ++i) {
-        pinMode(xshutpins[i], OUTPUT);
-        digitalWrite(xshutpins[i], LOW);
-    }
-    delay(10);
-    for (int8_t i=0; i<num_devices; ++i) {
-        digitalWrite(xshutpins[i], HIGH);
-    }
-    delay(10);
     
-    // Activate the first sensor
-    digitalWrite(xshutpins[1], LOW);
-    if (!tof0.begin(tof0_addr)) {
-        error_loop();
+    // for (int8_t i=0; i<num_sensors; ++i) {
+    //     pinMode(xshutpins[i], OUTPUT);
+    //     digitalWrite(xshutpins[i], LOW);
+    // }
+    // delay(10);
+    // for (int8_t i=0; i<num_sensors; ++i) {
+    //     digitalWrite(xshutpins[i], HIGH);
+    // }
+    // delay(10);
+    
+    // // Activate the first sensor
+    // digitalWrite(xshutpins[1], LOW);
+    // if (!tof0.begin(tof0_addr)) {
+    //     error_loop();
+    // }
+    // delay(10);
+
+    // // Activate the second sensor
+    // digitalWrite(xshutpins[1], HIGH);
+    // delay(10);
+    // if (!tof1.begin(tof1_addr)) {
+    //     error_loop();
+    // }
+    // delay(10);
+
+    // shudown all sensors
+    for (uint8_t i=0; i<num_sensors; ++i) {
+        digitalWrite(sensors[i].shutdown_pin, LOW);
     }
     delay(10);
 
-    // Activate the second sensor
-    digitalWrite(xshutpins[1], HIGH);
-    delay(10);
-    if (!tof1.begin(tof1_addr)) {
-        error_loop();
+    for (uint8_t i=0; i<num_sensors; ++i) {
+        digitalWrite(sensors[i].shutdown_pin, HIGH);
+        delay(10);
+        if (!sensors[i].psensor->begin(sensors[i].id, false, sensors[i].pwire, sensors[i].sensor_config)) {
+            error_loop();
+        }
+        
     }
+
+
+    // for (int8_t i=0; i<num_sensors; ++i) {
+    //     digitalWrite(xshutpins[i], HIGH);
+    //     delay(10);
+    //     if (!tofs[i].begin(tof_addrs[i])) {
+    //         error_loop(); // resets the device and continuously tries again
+    //     }
+    //     delay(10);
+    // }
 }
 
 void read_tof_sensors() {
-    tof0.rangingTest(&tofdata0);
-    tof1.rangingTest(&tofdata1);
-
-    /* Hardcode method */
-    
-    if (tofdata0.RangeStatus != 4) { // if not out of range
-        // msg.tof0 = tofdata0.RangeMilliMeter;
-        msg_arr.data[0] = tofdata0.RangeMilliMeter;
-    }
-    else {
-        // msg.tof0 = -1;
-        msg_arr.data[0] = -1;
-    }
-    if (tofdata1.RangeStatus != 4) {
-        // msg.tof1 = tofdata1.RangeMilliMeter;
-        msg_arr.data[1] = tofdata1.RangeMilliMeter;
-
-    }
-    else {
-        // msg.tof1 = -1;
-        msg_arr.data[1] = -1;
-    }
-
-
-    
-
-    /* Loop method! */
-    int i = 0;
-    for (auto& data_struct : data_structs) {
-        
-        Serial.println(data_struct.RangeStatus);
-        if (data_struct.RangeStatus != 4) {
-            msg_arr.data[i] = data_struct.RangeMilliMeter;
+    /* Read each sensor and store in data struct */
+    for (uint8_t i=0; i<num_sensors; ++i) {
+        tofs[i].rangingTest(&(data_structs[i]));
+        if (data_structs[i].RangeStatus != 4) {
+            msg_arr.data[i] = data_structs[i].RangeMilliMeter;
         }
         else {
             msg_arr.data[i] = -1;
         }
-        ++i;
     }
 }
 
@@ -235,16 +268,18 @@ void setup() {
 
     // msg.tof0 = 0.0;
     // msg.tof1 = 0.0;
-    msg_arr.data[0] = 10.0;
-    msg_arr.data[1] = 10.0;
+    msg_arr.data[0] = 0.0;
+    msg_arr.data[1] = 0.0;
     
 }
 
 void loop() {
+    Serial.println("Hello world");
     // Read from ToF sensors
     read_tof_sensors();
 
     Serial.println(msg_arr.data[0]);
+    Serial.println(msg_arr.data[1]);
 
     // // Check system state for connection status to micro_ros_agent
     // switch (system_state) {
